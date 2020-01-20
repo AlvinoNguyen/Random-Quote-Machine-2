4e84887d20729e14d8133952bb1e9660520d68de
Alvin Nguyen <alvinonguyen00@gmail.com>
2020-01-20 14:51:15 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const getNewColor = (oldColor) => {
+    const colors = [
+        '#cc0000ff', '#e69138ff', '#f1c232ff',
+        '#6aa84fff', '#3c78d8ff', '#674ea7ff',
+        '#990000ff', '#b45f06ff', '#bf9000ff',
+        '#38761dff', '#1155ccff', '#351c75ff',
+    ];
+    let newColor = colors[Math.floor(Math.random() * colors.length)];
+    while(newColor === oldColor)
+        newColor = colors[Math.floor(Math.random() * colors.length)];
+    return newColor;    
+}
+
+const colorPage = (color) => {
+    document.getElementById('app-container').style.backgroundColor = color;
+    document.querySelector('.quote-container').style.color = color;
+    const buttons = document.querySelector('.button-container').children;
+    for(let i = 0; i < buttons.length; i++)
+        buttons[i].style.backgroundColor = color;
+};
+
+const fetchQuotes = () => {
+    fetch('https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+    .then(res => res.json())
+    .then(data => {
+        const newColor = getNewColor();
+        colorPage(newColor);
+        document.querySelector('.quotetext').textContent = data.quoteText;
+        document.querySelector('.quoteauthor').textContent =
+        data.quoteAuthor ? data.quoteAuthor : 'Unknown';
+    })
+    .catch(err => { console.log(err); });
+};
+
+colorPage('#000000ff');
+fetchQuotes();
